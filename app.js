42ea871d3768b03740b812910b62398d9f57a79c
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-06 10:19:49 -0400

    ✨ Implementation of a User Routes and Controller, method: "signupUser" implemented.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 require('dotenv').config();
 require('./database/mongoose')
 
+// routers
+const userRouter =  require('./routers/user');;
+
 // models
 const User = require('./models/user');
 
@@ -9,20 +12,9 @@ const app = express();
 
 // use middlewares
 app.use(express.json());
+app.use('/api', userRouter);
 
 const PORT = process.env.PORT || 8002;
-const user = new User({
-  firstname: 'Jorge',
-  lastname: 'Mollericon',
-  email: 'jlmollericon@gmail.com',
-  password: '1234',
-  rol: 'admin'
-});
-
-user.save((err, user) => {
-  if (err) return console.log(err);
-  console.log(user)
-})
 
 app.listen(PORT, () => {
   console.log('Listen on port:', PORT);
