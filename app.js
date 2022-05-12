fe3699eeb95237435200495791b2e89b187ab8aa
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-12 10:04:17 -0400

    ⚡️ use cors in app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 require('dotenv').config();
 require('./database/mongoose')
 
 // routers
 const userRouter =  require('./routers/user');;
 
-// models
-const User = require('./models/user');
-
 const app = express();
 
 // use middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+
 app.use('/api', userRouter);
 
 const PORT = process.env.PORT || 8002;
